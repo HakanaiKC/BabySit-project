32a2c98b1d629d1f22384b25db2bddf114513387
Hakanai <cuonglkhe150595@fpt.edu.vn>
2021-10-20 11:09:57 +0700

    Cương update bài

--- BabySit/BabySit/Database Acess/DataAcess.cs
+++ BabySit/BabySit/Database Acess/DataAcess.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BabySit.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,18 +12,6 @@ namespace BabySit.Database_Acess
 {
     public class DataAcess
     {
-        //Khai bao đối tượng kết nối va thuc thi cac truy van
-        SqlConnection connection;
-        SqlCommand command;
-
-        string getConnectionString()
-        {
-            //Lay chuoi ket noi tu appsettings.json
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true)
-                .Build();
-            return configuration["ConnectionString:MyStoreDB"];
-        }
+        
     }
 }
