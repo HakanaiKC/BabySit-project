d60fc2c66a86ca73dcf29e8cc424acb9dedff36b
huyen2101 <huyenpthe150576@fpt.edu.vn>
2021-11-12 01:24:44 +0700

    Huyen up Search By Name

---
+++ BabySit/BabySit/Database Access/Search.cs
@@ -0,0 +1,19 @@
+﻿using BabySit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabySit.Database_Access
+{
+    public class Search
+    {
+        public IEnumerable<Skill> skills { get; set; }
+        public IEnumerable<User> users { get; set; }
+        public IEnumerable<Location> locations { get; set; }
+        public int ProvinceId { get; set; }
+        public string ProvinceName { get; set; }
+        public IEnumerable<UserSkill> userskills { get; set; }
+        
+    }
+}
