67f6f5bd339e15f5345526a1eaa100d70aa108ee
DatVT1108 <91654468+DatVT1108@users.noreply.github.com>
2021-11-13 06:25:18 +0700

    add iconfig

--- BabySit/BabySit/Models/ProjectPRNContext.cs
+++ BabySit/BabySit/Models/ProjectPRNContext.cs
@@ -44,11 +44,11 @@ namespace BabySit.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =(local); database = ProjectPRN;uid=sa;pwd=123456;");
-            }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration config = builder.Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("ProjectPRNDB"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
